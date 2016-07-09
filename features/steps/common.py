e7b9c531e066deef6a8e8c405adb9537f4ae1087
Ben Rogers <benrogers@mac.com>
2016-07-09 18:31:16 -0400

    Let the tests reuse the docker layer from test to test instead of redownloading tini

--- features/steps/common.py
+++ features/steps/common.py
@@ -3,6 +3,8 @@ import os
 import shutil
 import subprocess as sp
 
+TINI_VERSION = 'v0.9.0'
+
 @given(u'a docker with resu and tini')
 def step_impl(context):
     docker_name = 'resu-testrun-docker'
@@ -11,15 +13,21 @@ def step_impl(context):
     shutil.copy('../build/resu', os.path.join(docker_name, 'resu'))
     with open(os.path.join(docker_name, 'Dockerfile'), 'w') as f:
         f.write("""
-FROM debian:8.1
+FROM debian:8.5
 
-ENV TINI_VERSION v0.9.0
-ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
-RUN chmod +x /tini
+RUN apt-get update \
+ && apt-get install -y --no-install-recommends \
+        ca-certificates \
+        curl \
+ && rm -rf /var/lib/apt/lists/* \
+ && apt-get clean
+
+RUN curl -L https://github.com/krallin/tini/releases/download/{}/tini > /tini \
+ && chmod +x /tini
 ENTRYPOINT ["/tini", "--"]
 
 ADD resu /sbin/resu
-""")
+""".format(TINI_VERSION))
     assert 0 == sp.call(['docker', 'build', '-t', docker_name, docker_name])
     context.docker_name = docker_name
 
